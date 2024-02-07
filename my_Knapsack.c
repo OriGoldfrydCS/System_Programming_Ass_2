933addfab3643a1353f9e1888d88f7a5f2f24659
Ori Goldfryd <117982672+OriGoldfrydCS@users.noreply.github.com>
2024-02-07 03:03:20 +0200

    v1_7.2.2024

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -3,24 +3,21 @@
 
 int main() 
 {
-    char productsArray[] = {'A', 'B', 'C', 'D', 'E'};
+    char productsArray[] = {'a', 'b', 'c', 'd', 'e'};
     int values[N];
     int weights[N];
     int selected_bool[N];   // An array for track the selection status of each product
 
     for(int i = 0; i < N; i++) {
-        printf("Insert value for product %c: ", productsArray[i]);
         scanf("%d", &values[i]);  
-
-        printf("Insert weight for product %c: ", productsArray[i]);
         scanf("%d", &weights[i]);  
     }
 
     int maxProfit = knapSack(weights, values, selected_bool);  // Call the knapSack function
 
     printf("Maximum profit: %d\n", maxProfit);
-    printf("Items that give the maximum profit: ");
-    for (int i = N-1; i >= 0; i--) 
+    printf("Selected items: ");
+    for (int i = 0; i < N; i++) 
     {
         if (selected_bool[i] == TRUE) {
             printf("%c ", productsArray[i]);  // Print the selected products
